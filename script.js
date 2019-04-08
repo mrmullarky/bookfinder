c823cac77b028736dfff0d8fa93a18f1aa9c46a5
mrmullarky <conormullarky@gmail.com>
2019-04-08 12:11:03 +0100

    Initial Project Version

---
+++ script.js
@@ -0,0 +1,53 @@
+//API key variable not needed, kept for future updates
+let API_KEY = 'AIzaSyAtaFaElcUouUKFILmLK_2ErBMpzneBcYU';
+
+//Initialising variables
+let btn = document.getElementById("btn");
+const container = document.getElementsByClassName("container");
+const table = document.getElementById("myTable");
+table.setAttribute("id", "myTable");
+
+function searchBooks(){
+    //query is search box
+    let query = document.getElementById("query").value;
+
+    //Re-Initialises table and headers
+    table.innerHTML = "";
+    const heading = table.createTHead();
+    const head1 = document.createElement("TH");
+    head1.innerHTML = "Title";
+    heading.appendChild(head1);
+
+    const head2 = document.createElement("TH");
+    head2.innerHTML = "Author(s)";
+    heading.appendChild(head2);
+
+    const head3 = document.createElement("TH");
+    head3.innerHTML = "Date Published";
+    heading.appendChild(head3);
+
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}+inauthor:${query}`)
+        .then(data => data.json())
+        .then(data => {
+            
+            data.items.forEach((book) => {
+
+                var row = heading.insertRow();
+                var cell1 = row.insertCell();
+                cell1.innerHTML = `${book.volumeInfo.title}`;
+
+                let authors = book.volumeInfo.authors.join(", ");
+                var cell2 = row.insertCell();
+                cell2.innerHTML = `${authors}`;
+
+                let date = `${book.volumeInfo.publishedDate}`
+                if(date == "undefined") date = 'Unknown';
+                var cell3 = row.insertCell();
+                cell3.innerHTML = `${date}`;
+
+            });
+        })
+        .catch(err => console.log(err));
+}
+
+btn.addEventListener("click", searchBooks);
